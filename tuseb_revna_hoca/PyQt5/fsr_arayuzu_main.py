7a1d9229568ef539892c24e5fa0dba0e5833e5a1
Yunus Emre Ataş <130724958+yunusemre2334@users.noreply.github.com>
2024-03-27 01:17:09 +0300

    Update fsr_arayuzu_main.py

--- tuseb_revna_hoca/PyQt5/fsr_arayuzu_main.py
+++ tuseb_revna_hoca/PyQt5/fsr_arayuzu_main.py
@@ -1,29 +1,35 @@
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
-import serial
 import time 
 import sys 
-import numpy as np  
+import numpy as np 
+import smbus2 as smbus
 
-index_sozlugu = {"9": 1, "8": 0}
+
+#buraya adres degerleri fsr lerin indexlerine göre arduino ide üzerinde 0 dan 32 ye belirlenmeli ardından gönderilmeli
+def verileri_donustur(veri):
+    
+    fsr_reading = (veri[0] << 8) + veri[1] 
+    adresim = veri[2]
+    
+    return adresim, fsr_reading
 
 class ArduinoThread(QThread):
-    data_received = pyqtSignal(str)
+    data_received = pyqtSignal(tuple)
 
-    def __init__(self, parent=None):
+    def __init__(self,channel=1, parent=None):
         super(ArduinoThread, self).__init__(parent)
-        self.serial_port = serial.Serial("COM3", 9600, timeout=1)
+        self.bus = smbus.SMBus(channel)
+        
 
     def run(self):
         while True:
-            if self.serial_port.in_waiting:
-                data = self.serial_port.readline().decode('utf-8').strip()
-                self.data_received.emit(data)
+            if self.bus.in_waiting:
+                for addr in range(0,32):
+                    self.gelen_veri = self.bus.read_i2c_block_data(addr, 0, 3)
+                    self.data_received.emit(tuple(self.gelen_veri))
             time.sleep(0.1)
-
-    def send_data(self, data):
-        self.serial_port.write(data.encode('utf-8'))
 
 class Yunus_Emre(QWidget):
     def __init__(self):
@@ -39,9 +45,9 @@ class Yunus_Emre(QWidget):
         grid_layout = QGridLayout()
         grid_layout.setSpacing(0)
 
-        self.label_list = [QLabel(f"FSR{i+1}") for i in range(0, 16)]
+        self.label_list = [QLabel(f"FSR{i+1}") for i in range(0, 32)]
         
-        for row in range(0, 4):
+        for row in range(0, 8):
             for col in range(0, 4):
                 grid_layout.addWidget(self.label_list[row * 4 + col], row, col)
             
@@ -50,22 +56,17 @@ class Yunus_Emre(QWidget):
         self.arduino_thread.data_received.connect(self.veriyi_guncelle)
         
     def veriyi_guncelle(self):
-        fsr_degeri = self.arduino_thread.serial_port.readline().decode("utf-8").strip()
-        son_rakam = fsr_degeri[-1]
+        adres, fsr_degeri = verileri_donustur(self.arduino_thread.gelen_veri)
+                
+        if fsr_degeri:
+            fsr_degeri = int(fsr_degeri) 
+            scaled_value = int(np.interp(fsr_degeri, [0, 1023], [0, 255]))
 
-        if son_rakam in index_sozlugu:
-            print(fsr_degeri[:-1])
-            index = index_sozlugu[son_rakam]
-            
-            if len(fsr_degeri) > 1:
-                fsr_degeri = int(fsr_degeri[:-1]) * 10
-                scaled_value = int(np.interp(fsr_degeri, [0, 1023], [0, 255]))
-
-                blue_value = 255 - scaled_value
-                red_value = scaled_value
-                color = QColor(red_value, 0, blue_value)
-                self.label_list[index].setStyleSheet(f"background-color: {color.name()};")
-                self.label_list[index].setAlignment(Qt.AlignCenter)
+            blue_value = 255 - scaled_value
+            red_value = scaled_value
+            color = QColor(red_value, 0, blue_value)
+            self.label_list[adres].setStyleSheet(f"background-color: {color.name()};")
+            self.label_list[adres].setAlignment(Qt.AlignCenter)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
