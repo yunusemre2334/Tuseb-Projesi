ce719f30a335ea3a339852690904f22867561c4a
Yunus Emre Ataş <130724958+yunusemre2334@users.noreply.github.com>
2024-04-21 16:32:53 +0300

    Update fsr_arayuzu_buffer.py

--- tuseb_revna_hoca/PyQt5/fsr_arayuzu_buffer.py
+++ tuseb_revna_hoca/PyQt5/fsr_arayuzu_buffer.py
@@ -1,40 +1,38 @@
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
-import serial
 import time 
 import sys 
 import numpy as np 
 import smbus2 as smbus
 
-satir_sayisi = 32
-sutun_sayisi = 24
 
-matris = [[0] * sutun_sayisi for _ in range(satir_sayisi)]
+def convert_data(data):
+    fsr_list = []
+    for i in range(0,7):
+        fsr_reading = (data[i] << 8) + data[i+1]
+        fsr_list.append(fsr_reading)
 
-#sadece fsr değeri alınıyor diğer istenen değerler eklenebilir.
-def request_data(slave_address, channel):
-    bus = smbus.SMBus(channel)
-    data = bus.read_i2c_block_data(slave_address, 0, 2)
-    fsr_reading = (data[0] << 8) + data[1]
-    return fsr_reading
+    return fsr_list
+
 
 class ArduinoThread(QThread):
     data_received = pyqtSignal(str)
 
-    def __init__(self, parent=None):
+    def __init__(self,channel=1, parent=None):
         super(ArduinoThread, self).__init__(parent)
-        self.serial_port = serial.Serial("COM3", 9600, timeout=1)
+        self.bus = smbus.SMBus(channel)
+        self.buffer = [[i*j for j in range(1, 8)] for i in range(1, 32)]
 
     def run(self):
         while True:
-            if self.serial_port.in_waiting:
-                data = self.serial_port.readline().decode('utf-8').strip()
-                self.data_received.emit(data)
+            if self.bus.in_waiting:
+                for addr in range(0,32):
+                    data = self.bus.read_i2c_block_data(addr+8, 0, 24)
+                    self.buffer[addr] = convert_data(data)
+                    self.data_received.emit(self.buffer)
             time.sleep(0.1)
 
-    def send_data(self, data):
-        self.serial_port.write(data.encode('utf-8'))
 
 class Yunus_Emre(QWidget):
     def __init__(self):
@@ -46,13 +44,13 @@ class Yunus_Emre(QWidget):
         self.setGeometry(200, 200, 1000, 800) 
         self.setWindowTitle("FSR Arayüzü")
         
+
         grid_layout = QGridLayout()
         grid_layout.setSpacing(0)
 
-        #fsr sayısını arttırmak için range kısmından 16 değeri arttırılabilir.
-        self.label_list = [QLabel(f"FSR{i+1}") for i in range(0, 16)]
+        self.label_list = [QLabel(f"FSR{i+1}") for i in range(0, 32)]
         
-        for row in range(0, 4):
+        for row in range(0, 8):
             for col in range(0, 4):
                 grid_layout.addWidget(self.label_list[row * 4 + col], row, col)
             
@@ -60,24 +58,22 @@ class Yunus_Emre(QWidget):
 
         self.arduino_thread.data_received.connect(self.veriyi_guncelle)
         
-    def veriyi_guncelle(self, fsr_degeri):
-        #surekli gelen her fsr değeri yazılıyor mu kontrol edilmeli test edilirken.
-        for i in range(satir_sayisi):
-            for j in range(sutun_sayisi):
-                matris[i][j] = fsr_degeri
-        #range(3) -> matrisin 24 byte lık her satırının ilk 3 elemanı fsr değeri olarak düşünüldü eğer farklı ise değiştirilebilir. 
-        for index in range(32):
-            fsr_degeri = int(''.join([str(matris[index][k]) for k in range(3)]))
+    def veriyi_guncelle(self):
+        fsr_list = self.arduino_thread.buffer
+        for i in range(0,32):
+            for j in range(0,7):
+                for k in range(0,4):
+                    fsr_degeri = fsr_list[i][k]
+                
+                    if fsr_degeri:
+                        fsr_degeri = int(fsr_degeri) 
+                        scaled_value = int(np.interp(fsr_degeri, [0, 1023], [0, 255]))
 
-            if len(str(fsr_degeri)) > 1:
-                fsr_degeri = int(str(fsr_degeri)[:-1]) * 10
-                scaled_value = int(np.interp(fsr_degeri, [0, 1023], [0, 255]))
-
-                blue_value = 255 - scaled_value
-                red_value = scaled_value
-                color = QColor(red_value, 0, blue_value)
-                self.label_list[index].setStyleSheet(f"background-color: {color.name()};")
-                self.label_list[index].setAlignment(Qt.AlignCenter)
+                        blue_value = 255 - scaled_value
+                        red_value = scaled_value
+                        color = QColor(red_value, 0, blue_value)
+                        self.label_list[i * 4 + k].setStyleSheet(f"background-color: {color.name()};")
+                        self.label_list[i * 4 + k].setAlignment(Qt.AlignCenter)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
